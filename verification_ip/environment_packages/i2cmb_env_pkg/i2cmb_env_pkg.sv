b92787bb60e375a28c7c14ac069077b892023f10
epmurphy <epmurphy@ncsu.edu>
2022-03-02 14:27:57 -0500

    Added project structure for layered testbench

--- verification_ip/environment_packages/i2cmb_env_pkg/i2cmb_env_pkg.sv
+++ verification_ip/environment_packages/i2cmb_env_pkg/i2cmb_env_pkg.sv
@@ -1,3 +1,10 @@
 package i2cmb_env_pkg;
     `include "src/i2c_op_t.svh"
+    `include "src/i2cmb_test.svh"
+    `include "src/i2cmb_generator.svh"
+    `include "src/i2cmb_env_configuration.svh"
+    `include "src/i2cmb_environment.svh"
+    `include "src/i2cmb_predictor.svh"
+    `include "src/i2cmb_scoreboard.svh"
+    `include "src/i2cmb_coverage.svh"
 endpackage
---
+++ verification_ip/interface_packages/i2c_pkg/i2c_pkg.sv
@@ -0,0 +1,7 @@
+package i2c_pkg;
+    `include "src/i2c_configuration.svh"
+    `include "src/i2c_agent.svh"
+    `include "src/i2c_driver.svh"
+    `include "src/i2c_monitor.svh"
+    `include "src/i2c_transaction.svh"
+endpackage
