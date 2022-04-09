eda479bdc995691515cdaf1ea55706c7c7d9c27c
epmurphy <epmurphy@ncsu.edu>
2022-04-09 13:35:33 -0400

    Project 3 commit

--- verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_test_regs.svh
+++ verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_test_regs.svh
@@ -1,27 +1,72 @@
+import wb_pkg::*;
+
 class i2cmb_test_regs extends ncsu_component;
 
-    i2cmb_env_configuration cfg;
-    i2cmb_environment env;
-    i2cmb_generator gen;
+    virtual wb_if bus;
+    //shorthands for wishbone register offsets
+    parameter CSR=2'b00;
+    parameter DPR=2'b01;
+    parameter CMDR=2'b10;
+    parameter FSMR=2'b11;
+
+    bit[7:0] wb_out;
 
     function new(string name = "", ncsu_component_base  parent = null);
         super.new(name, parent);
-        cfg = new("cfg");
-        env = new("env", this);
-        env.set_configuration(cfg);
-        env.build();
-        gen = new("gen", this);
-        gen.set_wb_agent(env.get_wb_agent());
-        gen.set_i2c_agent(env.get_i2c_agent());
-
-        //notify generator of device setup
-        //another way of doing this would be using the factory
-        //for a unique generator
-        gen.cei = cfg.core_enable_interrupts;
     endfunction
 
     virtual task run();
-        env.run();
-        gen.run();
+        if ( !(ncsu_config_db#(virtual wb_if)::get("tst.env.wb_agent", this.bus))) begin
+            $display("wb_agent::ncsu_config_db::get() call for BFM handle failed for name: %s ",get_full_name());
+        end
+        $display("I2CMB_TEST_REGS: Acquired handle to Wishbone bus");
+
+        bus.wait_for_reset();
+        bus.reset_bus();
+
+        $display("I2CMB_TEST_REGS: Checking default register values");
+        bus.master_read(CSR, wb_out);
+        assert(wb_out == 8'h00) else begin
+            ncsu_errors++;
+            $display("I2CMB_TEST_REGS: CSR Default Value Wrong");
+        end
+
+        bus.master_read(DPR, wb_out);
+        assert(wb_out == 8'h00) else begin
+            ncsu_errors++;
+            $display("I2CMB_TEST_REGS: DPR Default Value Wrong");
+        end
+
+        bus.master_read(CMDR, wb_out);
+        assert(wb_out == 8'h80) else begin
+            ncsu_errors++;
+            $display("I2CMB_TEST_REGS: CMDR Default Value Wrong");
+        end
+
+        bus.master_read(FSMR, wb_out);
+        assert(wb_out == 8'h00) else begin
+            ncsu_errors++;
+            $display("I2CMB_TEST_REGS: FSMR Default Value Wrong");
+        end
+
+        $display("I2CMB_TEST_REGS: Register Default Values Pass");
+
+        $display("I2CMB_TEST_REGS: Testing CSR Write Permissions at Startup");
+        bus.master_write(CSR, 8'b00111111);
+        bus.master_read(CSR, wb_out);
+        assert(wb_out == 8'h00) else begin
+            ncsu_errors++;
+            $display("I2CMB_TEST_REGS: CSR Bit Permissions at Startup Incorrect");
+        end
+
+        $display("I2CMB_TEST_REGS: CSR[5:0] correctly RO before core startup");
+
+        $display("*----------------------------------------------*");
+        $display("Tests Finished");
+        $display("Warnings %d, Errors %d, Fatals, %d", ncsu_warnings, ncsu_errors, ncsu_fatals);
+        if((ncsu_warnings == 0) && (ncsu_errors == 0) && (ncsu_fatals == 0)) $display("ALL TESTS PASSED");
+        else $display("TESTS FAILED OR WARNINGS TRIGGERED");
+        $display("*----------------------------------------------*");
+        $finish;
     endtask
 endclass
